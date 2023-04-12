6373009a3ffb6e9409fd80854a0470295835b1ad
sueyvid <sueyvidr8@gmail.com>
2023-04-11 22:25:45 -0300

    q18

--- q18.c
+++ q18.c
@@ -5,29 +5,42 @@ void armazena_valores(float **mat, int nl, int nc){
     int i, j;
     for (i=0; i<nl; i++){
         for(j=0; j<nc; j++){
-            scanf("%d", &mat[i][j]);
+            scanf("%f", &mat[i][j]);
         }
     }
 }
 
 void imprime_valores(float **mat, int nl, int nc){
     int i, j;
-    for (i=0; i<nl; i++){
+    for(i=0; i<nl; i++){
         for(j=0; j<nc; j++){
-            printf("%d ", mat[i][j]);
+            printf("%f ", mat[i][j]);
         }
+        printf("\n");
     }
-    printf("\n");
 }
 
 void multiplica_matrizes(float **A, float **B, float **C, int nl_A, int nc_A, int nc_B){
-    
+    int i, j, k;
+
+    for(i=0; i<nl_A; i++){
+        for(j=0; j<nc_B; j++){
+            for(k=0; k<nc_A; k++){
+                C[i][j] += A[i][k] * B[k][j];
+            }
+        }
+    }
 }
 
 void main(){
     float **A, **B, **C;
     int nl_A, nc_A, nl_B, nc_B;
     int i, j;
+    
+    nl_A = 3;
+    nc_A = 4;
+    nl_B = 4;
+    nc_B = 3;
     
     // Alocação de A
     A = (float**) malloc(nl_A * sizeof(float*));
@@ -44,7 +57,7 @@ void main(){
     }
     
     // Ajustes de A[i]
-    for(i=0; i<nl_A; i++){
+    for(i=1; i<nl_A; i++){
         A[i] = A[i-1] + nc_A;
     }
     
@@ -65,12 +78,12 @@ void main(){
     // Alocação de B[0]
     B[0] = (float*) malloc(nl_B * nc_B * sizeof(float));
     if(B[0] == NULL){
-        printf("Alocação de A[0] não funcionou!");
+        printf("Alocação de B[0] não funcionou!");
         exit(0);
     }
     
     // Ajustes de B[i]
-    for(i=0; i<nl_B; i++){
+    for(i=1; i<nl_B; i++){
         B[i] = B[i-1] + nc_B;
     }
     
@@ -81,7 +94,48 @@ void main(){
         }
     }
     
+    // Alocação de C
+    C = (float**) malloc(nl_A * sizeof(float*));
+    if (C == NULL){
+        printf("Alocação de C não funcionou!");
+        exit(0);
+    }
+
+    // Alocação de C[0]
+    C[0] = (float*) malloc(nl_A * nc_B * sizeof(float));
+    if (C[0] == NULL){
+        printf("Alocação de C[0] não funcionou!");
+        exit(0);
+    }
+
+    // Ajuste de C[i]
+    for (i=1; i<nl_A; i++){
+        C[i] = C[i-1] + nc_B;
+    }
+
+    // Atribuição de 0
+    for(i=0; i<nl_A; i++){
+        for(j=0; j<nc_B; j++){
+            C[i][j] = 0;
+        }
+    }
+    
+    // matriz A
+    printf("Digite as dimensões de A: ");
+    scanf("%d", &nl_A);
+    scanf("%d", &nc_A);
+    printf("Digite os valores de A:\n");
+    armazena_valores(A, nl_A, nc_A);
+    
+    // matriz B
+    printf("Digite as dimensões de B: ");
+    scanf("%d", &nl_B);
+    scanf("%d", &nc_B);
+    printf("Digite os valores de B:\n");
+    armazena_valores(B, nl_B, nc_B);
+    
     multiplica_matrizes(A, B, C, nl_A, nc_A, nc_B);
     
-    imprime_valores(A, nl_A, nc_A);
+    printf("Resultado:\n");
+    imprime_valores(C, nl_A, nc_B);
 }
